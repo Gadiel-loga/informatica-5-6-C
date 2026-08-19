3e4271eacd3bc29979621668a6124d077ea0c41e
Gadiel-loga <christiangadielloga@gmail.com>
2026-08-19 20:18:43 +0000

    currency

--- currency.py
+++ currency.py
@@ -1,14 +1,13 @@
 def main():
-    🇨🇴 = float(input("What do you have left in pesos? "))
-    🇵🇪 = float(input("What do you have left in soles? "))
-    🇧🇷 = float(input("What do you have left in reais? "))
+    CO = float(input("What do you have left in pesos? "))
+    PE = float(input("What do you have left in soles? "))
+    BR = float(input("What do you have left in reais? "))
 
-    print("Colombian pesos")
-    print("Peruvian soles")
-    print("Brazilian reais")
+    mxn = (CO * 0.0054) + (PE * 5.07) + (BR * 3.28)
+    usd = mxn / 17.06
 
-
-
+    print("MXN: ", round(mxn, 2))
+    print("USD: ", round(usd, 2))
 
 if __name__ == "__main__":
     main()
