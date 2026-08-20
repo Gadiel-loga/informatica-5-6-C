6e8295462b6b06918052f5cd154aae562fe2db8b
Gadiel-loga <christiangadielloga@gmail.com>
2026-08-20 16:40:49 +0000

    variables

---
+++ update-variable.py
@@ -0,0 +1,35 @@
+def main():
+
+    # Augmented Assignment Operator
+    money = 5
+    money += 10
+    print(money) # This will print 15
+
+    # Substraction Assignment Operator
+    calories = 60
+    calories -= 25
+    print(calories) # This will print 35
+
+    # Multiply Assignment Operator
+    skill = 10
+    skill *= 20
+    print(skill) # This will print
+
+    text = "ImVictor"
+    text *= 20
+    print(text)
+
+    # Division Assignment Operator
+    pizzas = 8
+    people = int(input("Number of people at the pizza party: "))
+    pizzas /= people
+    print(pizzas)
+
+    # Modulus Assignment Operator
+    leftover = 8
+    leftover %= people
+    print(leftover)
+
+if __name__ == "__main__":
+    main()
+
