6838041789b1343fae17f78a288522aa34d9a252
Gadiel-loga <christiangadielloga@gmail.com>
2026-08-21 16:58:29 +0000

    transistors

--- weekly-checkpoint-2/moores.py
+++ weekly-checkpoint-2/moores.py
@@ -1,11 +1,13 @@
 def main():
     years = int(input("Enter a number of years into the future: "))
     transistors = 17.8
-    transistors *= 2 **  (years / 2)
-    print(transistors, "Billions")
-
-
-
+    current_year = 2026
+    
+    if (current_year + years) >= 2030:
+        print("the law is not valid.")
+    else:
+        transistors *= 2 ** (years / 2)
+        print(transistors, "Billions")
 
 if __name__ == "__main__":
     main()
