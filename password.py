2de839485b032abff96679b542187d59b431600e
Gadiel-loga <christiangadielloga@gmail.com>
2026-08-24 19:35:58 +0000

    password

---
+++ password.py
@@ -0,0 +1,14 @@
+def main():
+    password = "1nf0rm4t1c4"
+
+
+    attempt = input("Enter password: ")
+
+    if attempt == password:
+        print("Correct password.")
+
+    print("See you later")
+
+
+if __name__ == "__main__":
+    main()
