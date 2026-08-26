68f6a49bc83ea57bdba4b5f57518dca44cc9db21
Gadiel-loga <christiangadielloga@gmail.com>
2026-08-26 20:12:04 +0000

    calculator

---
+++ if-exercises.py
@@ -0,0 +1,29 @@
+def main():
+    intenger = int(input("Enter integer number: "))
+
+    if intenger < 0:
+
+        total = intenger * (-1)
+        print("", total)
+    elif intenger >= 0:
+        print("", intenger)
+
+    x = int(input("Enter number for x: "))
+    y = int(input("Enter number for y: "))
+    operation = input("Enter the operation for x & y: ")
+
+    if operation == "add":
+        product = x + y
+        print("", product)
+    elif operation == "subtract":
+        product = x - y
+        print("", product)
+    elif operation == "multiply":
+        product = x * y
+        print("", product)
+
+    
+
+
+if __name__ == "__main__":
+    main()
