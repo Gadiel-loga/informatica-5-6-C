451601e39055370fed7465aa44d6e3ce81bdcf1d
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-03 20:20:17 +0000

    a

--- guess-number.py
+++ guess-number.py
@@ -1,39 +1,24 @@
 import random
 
 def main():
-
-    num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
     nombre = input("Hello!, can I know who are you? ").title().strip()
-    print(f"Well, {nombre}, I am thinking of a number between 1 and 100, why you don't try to guess it?.")
-        guess = random.choice(num)
-        if guess == "1":
-            
-        elif guess == "2":
-        elif guess == "3":
-        elif guess == "4":
-        elif guess == "5":
-        elif guess == "6":
-        elif guess == "7":
-        elif guess == "8":
-        elif guess == "9":
-        elif guess == "10":
-
-
-
-    faa = ""
-
-        while answer != "Yes!":
-            answer = input("Are we there yet? ").strip().title()
-            if answer == "Yes":
-                followup = input("Really? ").strip().title()
-            if followup == "Yes!":
-                break
+    
 
 
 
 
+    print(f"Well, {nombre}, I am thinking of a number between 1 and 100, why you don't try to guess it?")
 
+    num = random.randint(1,100)
+    guess = 45
+
+    while guess != num:
+        guess = int(input("Why you don't try to guess it: "))
+        if guess > num:
+            print("That's too big, why you don't try to guess it: ")
+        elif guess < num:
+            print("That's too small, why you don't try to guess it: ?")
+    print(f"Great job, {nombre}, you could guess my number")
 
 
 if __name__ == "__main__":
