cc7e1b4253cec38089e471178575e2815c6e1b79
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-04 17:30:24 +0000

    f

---
+++ learning-app.py
@@ -0,0 +1,19 @@
+import random
+
+def main():
+    print("Enigmexis")
+
+    problem1 = random.randint(10,99)
+    problem2 = random.randint(10,99)
+    solution = problem1 + problem2
+
+    answer = int(input("If you cannot solve this operation ))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
