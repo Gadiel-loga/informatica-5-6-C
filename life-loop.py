eabf52a4c1964c595958b96e5ca877664e00e796
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-09 02:45:50 +0000

    asasa

---
+++ life-loop.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+def main():
+    uplift = ["Keep Going!", "Don't give up!", "You're doing great!", "Small progress is still progress!", "You got this!"]
+
+    uplift1 = ["Almost there!", "The final stretch!"]
+
+    dontstop = ["Even Einstein took breaks!", "Take a moment to breathe!", "Don't stop now!"]
+
+    assignments = int(input("How many assignments do you have?"))
+
+    work = ""
+
+    message = ""
+    uplift2 = ""
+    dont = ""
+    while assignments != 0:
+
+        work = int(input("How many assignments have you completed?"))
+        message = random.choice(uplift)
+        uplift2 = random.choice(uplift1)
+        dont = random.choice(dontstop)
+        assignments -= work
+
+        if assignments >= 3:
+            print(f"{message}")
+
+        elif assignments < 3:
+            print(f"{uplift2}")
+
+        if assignments == 0:
+            print("You finished!")
+            break
+
+        if work == 0:
+            print(f"{dont}")
+
+        time.sleep(300)
+
+
+
+if __name__ == "__main__":
+    main()
