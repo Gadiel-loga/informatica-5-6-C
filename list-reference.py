357bea47bb4fbbddd10daa8de96367796e00450c
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-11 17:30:44 +0000

    s

--- list-reference.py
+++ list-reference.py
@@ -1,102 +1,74 @@
 def main():
+    # min, max, sum
     num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    ads = max(num)
+    ads = max(num) #highest number
     print(ads)
-    ads2 = sum(num)
+    ads2 = sum(num) #plus all numbers
     print(ads2)
-    ads3 = min(num)
+    ads3 = min(num) #minimum
     print(ads3)
 
- def main():
-#sort
-numbers = [1, 4, 5, 7, 9, 3]
-numbers.sort()
-print(numbers)
+    #sort: ordena segun el orden general
+    numbers = [1, 4, 5, 7, 9, 3]
+    numbers.sort()
+    print(numbers)  #Basado en los primeros numeros
 
-items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
-items.sort()
-print(items)
+    items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
+    items.sort() #Basado en las primeras letras del abecedario
+    print(items)
 
-letters = ["b", "a", "d", "c"]
-letters.sort(reverse=True)
-print(letters)
+    letters = ["b", "a", "d", "c"]
+    letters.sort(reverse=True) #reverse=True 
+    print(letters)
 
-words = ['banana', 'pie', 'apple']
-sorted_words = sorted(words, key=len)
-print(sorted_words)
-
-
-#append and insert
-fruits=["Apple","Orange","Grapes"]
-fruits.append("Banana") #adds to the list
-print(fruits)
-fruits1= ["Apple","Orange","Grapes"]
-fruits1.insert(2, "banana") #inserts in a specific place of the list
-print(fruits1)
-#len al parecer
-
-mylist = ["pencil", "computer", "shirt", "phone", "paper"]
-print(len(mylist)) #counts items on the list
-
-# min, max, sum
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-ads = max(num) #highest number
-print(ads)
-ads2 = sum(num) #logic
-print(ads2)
-ads3 = min(num) #minimum
-print(ads3)
-
-#
-list = ["Rojo","amarillo","Verde","naranja","Azul"]
-print("lista", list)
-
-list.pop(1)
-print("con pop(1):", list)
-
-list.remove("Rojo")
-print("con remove:",'Rojo')
-
-if __name__ == "__main__":
-main()
-Solano, Abdiel Eduardo , Cabrera, Cameron Trinidad , Andalon, Enrique Zarish1 more
-5 SEM 26-FL-INFORMATICA MODULO 5 Y 6
-Sep 10, 2026 at 2:12pm
+    words = ['banana', 'pie', 'apple']
+    sorted_words = sorted(words, key=len) #key=len
+    print(sorted_words)
 
 
-def main():
+    #append and insert
+    fruits=["Apple","Orange","Grapes"]
+    fruits.append("Banana") #adds to the list
+    print(fruits)
+    fruits1= ["Apple","Orange","Grapes"]
+    fruits1.insert(2, "banana") #inserts in a specific place of the list
+    print(fruits1)
+    #len al parecer
 
-numbers = [1, 4, 5, 7, 9, 3]
-numbers.sort()
-print(numbers)
+    mylist = ["pencil", "computer", "shirt", "phone", "paper"]
+    print(len(mylist)) #counts items on the list
 
-items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
-items.sort()
-print(items)
+    list = ["Rojo","amarillo","Verde","naranja","Azul"]
+    print("lista", list)
 
-letters = ["b", "a", "d", "c"]
-letters.sort(reverse=True)
-print(letters)
+    list.pop(1)
+    print("con pop(1):", list)
 
-words = ['banana', 'pie', 'apple']
-sorted_words = sorted(words, key=len)
-print(sorted_words)
+    list.remove("Rojo")
+    print("con remove:",'Rojo')
 
-if __name__ == "__main__":
-main()
-AZ
-Andalon, Enrique Zarish, Cabrera, Cameron Trinidad , Solano, Abdiel Eduardo 1 more
-5 SEM 26-FL-INFORMATICA MODULO 5 Y 6
-Sep 10, 2026 at 2:10pm
+    numbers = [1, 4, 5, 7, 9, 3]
+    numbers.sort()
+    print(numbers)
 
+    items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
+    items.sort()
+    print(items)
 
-def main():
+    letters = ["b", "a", "d", "c"]
+    letters.sort(reverse=True)
+    print(letters)
 
-fruits=["Apple","Orange","Grapes"]
-fruits.append("Banana")
-print(fruits)
-fruits1= ["Apple","Orange","Grapes"]
-fruits1.insert(2, "banana")
-print(fruits1)
+    words = ['banana', 'pie', 'apple']
+    sorted_words = sorted(words, key=len)
+    print(sorted_words)
+
+    fruits=["Apple","Orange","Grapes"]
+    fruits.append("Banana")
+    print(fruits)
+    fruits1= ["Apple","Orange","Grapes"]
+    fruits1.insert(2, "banana")
+    print(fruits1)
+
 if __name__ =="__main__":
-main()
+    main()
