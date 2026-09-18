71311b0ad5c62546efb47c34022d90c52c2766bf
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-18 16:16:50 +0000

    doctrine

---
+++ for-intro.py
@@ -0,0 +1,12 @@
+def main():
+    Doctrine = ["Faith", "Repentance", "Baptism", "Confirmation", "Persevere to the end"]
+
+    for intro in Doctrine:
+        print(intro)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
