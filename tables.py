e6de1513ab58b0ea69f28479c00a19bf099757c6
Gadiel-loga <christiangadielloga@gmail.com>
2026-09-23 20:20:07 +0000

    x

---
+++ tables.py
@@ -0,0 +1,12 @@
+def main():
+    number = int(input("Give me a number little buddy from 1 to 10: "))
+
+    for number in range(table):
+        print(f"Here is the {number} times table")
+        print((number + 1) f" times {number} is "(number * ))
+
+
+
+
+if __name__ == "__main__":
+    main()
